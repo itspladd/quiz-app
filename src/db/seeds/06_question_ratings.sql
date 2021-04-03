c851fa683bd2c0f3628d2fe4c8361166062e2817
Paul Ladd <vlendle@gmail.com>
2021-04-03 15:27:28 +0000

    Add a rating with no comment so we have a test case for that

--- src/db/seeds/06_question_ratings.sql
+++ src/db/seeds/06_question_ratings.sql
@@ -3,4 +3,5 @@ VALUES
 (1, 1, 5, 'incredible question. a masterpiece'),
 (2, 1, 1, 'paul what are you doing'),
 (3, 6, 2, 'this sounds scary! I was too scared to finish the quiz')
+(3, 4, 5, NULL)
 ;
