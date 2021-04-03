96fab5db091b6a7d761dbd2a2da11ea23b0e6e40
Paul Ladd <vlendle@gmail.com>
2021-04-03 15:25:36 +0000

    Add a new dummy user for testing and seeds for question ratings

--- src/db/seeds/01_users.sql
+++ src/db/seeds/01_users.sql
@@ -1,4 +1,5 @@
 INSERT INTO users (username, password, email)
 VALUES
 ('pladd', 'password', 'pladd@radd.cad'),
-('reggi', 'password', 'regg@egg.megg');
+('reggi', 'password', 'regg@egg.megg'),
+('timid_terry', 'password', 'terry@mary.berry');
