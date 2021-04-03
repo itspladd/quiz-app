063454130deba9bfbab024516162430d51990021
Paul Ladd <vlendle@gmail.com>
2021-04-03 02:51:27 +0000

    Adding seeds for questions and answers

---
+++ src/db/seeds/05_answers.sql
@@ -0,0 +1,22 @@
+INSERT INTO answers (question_id, body, is_correct)
+VALUES
+(1, '(C) No, he is an obligate fungivore', true),
+(1, 'Yes, he loves spaghetti but only spaghetti', false),
+(1, 'Yes, Mario eats a wide variety of foods', false),
+(2, 'Gross. Mushrooms are gross.', false),
+(2, '(C)Only if the spaghetti and the sauce are all made of mushrooms', true),
+(2, 'Yes, Mario loves eating Wheat Pasta with Tomato Sauce', false),
+(3, 'No, he absorbs its power through his skin', false),
+(3, 'No, he absorbs its power through his soul', true),
+(3, '(C) He eats them, but only because they are also a form of mushroom.', true),
+(4, 'Yes', false),
+(4, 'Yes but I don''t have the key :(', true),
+(4, '(C)No? Why would I have that', true),
+(5, '(C)0-10', true),
+(5, '10-30', false),
+(5, '30-180', false),
+(5, 'psh. try me.', false),
+(6, 'Yes. I can fight Pluto. I will kick Pluto, the planet, with my feet.', false),
+(6, '(C)Hah! I''d like to see it try--oh no. It''s behind me, isn''t it.', true),
+(6, 'What are you TALKING about', false)
+;
