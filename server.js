d18be9a52ee15a5564ec53dd19547a60f08e8ad3
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-02 20:52:04 -0400

    Remove sass-middleware import

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 const express = require("express");
 const bodyParser = require("body-parser");
-const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require("morgan");
 
