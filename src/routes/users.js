b974d0fe2651ec268ec6c2bc706b674770d2b176
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-02 21:33:18 -0400

    Temp remove incomplete queries

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,37 +5,24 @@ module.exports = (db) => {
 
   // /users/dashboard
   router.get("/dashboard", (req, res) => {
-    db.query("SELECT * FROM users;")
-      .then(data => {
-        const users = data.rows;
-        return { users };
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    // db.query("SELECT * FROM users;")
+    //   .then(data => {
+    //     const users = data.rows;
+    //     return { users };
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
+    res.render("dashboard");
   });
 
   // /users/:userid
   router.get("/:userid", (req, res) => {
-    db.query("SELECT...")
-      .then()
-      .catch();
-  });
-
-  // /users
-  router.get("/", (req, res) => {
-    db.query("SELECT * FROM users;")
-      .then(data => {
-        const users = data.rows;
-        return { users };
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    // db.query("SELECT...")
+    //   .then()
+    //   .catch();
   });
 
   return router;
