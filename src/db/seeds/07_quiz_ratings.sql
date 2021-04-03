238b82bfbf6eaf5d6a1b9a2e20f22bc8395ef547
Paul Ladd <vlendle@gmail.com>
2021-04-03 15:31:29 +0000

    Add quiz_ratings seeds

---
+++ src/db/seeds/07_quiz_ratings.sql
@@ -0,0 +1,9 @@
+INSERT INTO quiz_ratings (user_id, quiz_id, rating, comment)
+VALUES
+(1, 1, 5, 'perfect quiz'),
+(1, 2, 5, NULL),
+(2, 1, 2, 'paul. come on.'),
+(3, 1, 5, 'wow! i learned a lot about Mario, and it wasn''t scary!'),
+(2, 2, 1, 'paul you realize that it''s YOU saying these things about your own db seeds right'),
+(3, 2, 1, NULL)
+;
