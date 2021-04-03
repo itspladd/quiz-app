7feb529d394e075074c9de6eba48abce58cde919
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-03 13:46:25 -0400

    Fix linting

--- src/bin/resetdb.js
+++ src/bin/resetdb.js
@@ -1,11 +1,11 @@
 // Copied from https://github.com/lighthouse-labs/node-skeleton/blob/master/bin/resetdb.js
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // other dependencies
-const fs = require('fs');
-const chalk = require('chalk');
-const Client = require('pg-native');
+const fs = require("fs");
+const chalk = require("chalk");
+const Client = require("pg-native");
 
 // PG connection setup
 const connectionString = process.env.DATABASE_URL ||
@@ -13,23 +13,23 @@ const connectionString = process.env.DATABASE_URL ||
 const client = new Client();
 
 // Loads the schema files from db/schema
-const runSchemaFiles = function () {
-  console.log(chalk.cyan(`-> Loading Schema Files ...`));
-  const schemaFilenames = fs.readdirSync('./src/db/schema');
+const runSchemaFiles = function() {
+  console.log(chalk.cyan("-> Loading Schema Files ..."));
+  const schemaFilenames = fs.readdirSync("./src/db/schema");
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./src/db/schema/${fn}`, 'utf8');
+    const sql = fs.readFileSync(`./src/db/schema/${fn}`, "utf8");
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
 };
 
-const runSeedFiles = function () {
-  console.log(chalk.cyan(`-> Loading Seeds ...`));
-  const schemaFilenames = fs.readdirSync('./src/db/seeds');
+const runSeedFiles = function() {
+  console.log(chalk.cyan("-> Loading Seeds ..."));
+  const schemaFilenames = fs.readdirSync("./src/db/seeds");
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./src/db/seeds/${fn}`, 'utf8');
+    const sql = fs.readFileSync(`./src/db/seeds/${fn}`, "utf8");
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
