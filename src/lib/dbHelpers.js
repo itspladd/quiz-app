39d1f875115449cba6bbd2a695bd9abb2037ea64
Paul Ladd <vlendle@gmail.com>
2021-04-03 17:36:24 +0000

    Add first dbHelpers function, getUserById, plus documentation

--- src/lib/dbHelpers.js
+++ src/lib/dbHelpers.js
@@ -1,4 +1,16 @@
-const db = require('./db');
+const db = require("./db");
 
-db.query('SELECT * FROM users')
-.then(rows => console.log(rows));
+// USING dbHelpers:
+// All queries are routed through the query(string, param) function in db.js
+// db.query returns rows of data by default, no need to do res.rows
+
+module.exports = { 
+  getUserById: function(id) {
+    const queryString = `SELECT *
+      FROM users
+      WHERE id = $1`;
+    const queryParams = [id];
+    return db.query(queryString, queryParams)
+    .catch(err => console.error(err));
+  }
+};
