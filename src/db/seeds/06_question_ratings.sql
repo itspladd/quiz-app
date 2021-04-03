f018a5c677285bbeae7c13571d8a0cc72b35bba3
Paul Ladd <vlendle@gmail.com>
2021-04-03 15:28:06 +0000

    Fix missing comma causing db reseed to break

--- src/db/seeds/06_question_ratings.sql
+++ src/db/seeds/06_question_ratings.sql
@@ -2,6 +2,6 @@ INSERT INTO question_ratings (user_id, question_id, rating, comment)
 VALUES
 (1, 1, 5, 'incredible question. a masterpiece'),
 (2, 1, 1, 'paul what are you doing'),
-(3, 6, 2, 'this sounds scary! I was too scared to finish the quiz')
+(3, 6, 2, 'this sounds scary! I was too scared to finish the quiz'),
 (3, 4, 5, NULL)
 ;
