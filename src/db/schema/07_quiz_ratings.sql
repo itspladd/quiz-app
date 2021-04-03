6584f6edec67e418f0f0b8de3e5e29868044b3a4
Paul Ladd <vlendle@gmail.com>
2021-04-03 16:02:24 +0000

    Modify erd and quiz_ratings schema to store session ID alongside quiz ID (so we can see what kind of quiz results might have prompted a certain review). Add seeds for quiz sessions.

--- src/db/schema/07_quiz_ratings.sql
+++ src/db/schema/07_quiz_ratings.sql
@@ -4,6 +4,7 @@ CREATE TABLE quiz_ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  session_id INTEGER REFERENCES quiz_sessions(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL,
   comment VARCHAR(255)
 );
