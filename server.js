5ab6b5baf834e2a48bc2d48ae82005ca78c39de0
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-04 02:27:22 -0400

    Add featured quizzes

--- server.js
+++ server.js
@@ -223,17 +223,11 @@ app.get("/404", (req, res) => {
 // Home page
 app.get("/", (req, res) => {
   console.log('made it here')
+  // Top 3 featured
   const quizData = [
     { id: "1", title: "Quiz Name", description: "This is the description."},
     { id: "2", title: "Quiz Name", description: "This is the description."},
     { id: "3", title: "Quiz Name", description: "This is the description."},
-    { id: "4", title: "Quiz Name", description: "This is the description."},
-    { id: "5", title: "Quiz Name", description: "This is the description."},
-    { id: "6", title: "Quiz Name", description: "This is the description."},
-    { id: "7", title: "Quiz Name", description: "This is the description."},
-    { id: "8", title: "Quiz Name", description: "This is the description."},
-    { id: "9", title: "Quiz Name", description: "This is the description."},
-    { id: "10", title: "Quiz Name", description: "This is the description."}
   ]
   const {
     alerts,
