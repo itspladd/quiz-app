0c32882d92aa1b56e37e13932013654b910f523d
Paul Ladd <vlendle@gmail.com>
2021-04-04 23:52:45 +0000

    Update schemas with new character limits

--- src/db/seeds/03_quizzes.sql
+++ src/db/seeds/03_quizzes.sql
@@ -1,4 +1,4 @@
 INSERT INTO quizzes (author_id, category_id, title, description, public)
 VALUES
-(1, 2, 'How Well Do You Know Mario''s Dietary Restrictions?', 'description', TRUE),
-(2, 1, 'Do You Have What It Takes to Convince NASA to Reclassify Pluto as a Planet?', 'description', TRUE);
+(1, 2, 'Mario''s Dietary Restrictions', 'Do you know what Mario can and can''t eat?', TRUE),
+(2, 1, 'Make Pluto a Planet Again', 'Can you convince NASA to reclassify Pluto as a planet?', TRUE);
