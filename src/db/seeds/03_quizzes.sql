a5832febd80b91af284a0ad01dff4ebf1037c073
Paul Ladd <vlendle@gmail.com>
2021-04-04 22:37:06 +0000

    Update to make initial seeds public

--- src/db/seeds/03_quizzes.sql
+++ src/db/seeds/03_quizzes.sql
@@ -1,4 +1,4 @@
-INSERT INTO quizzes (author_id, category_id, title, description)
+INSERT INTO quizzes (author_id, category_id, title, description, public)
 VALUES
-(1, 2, 'How Well Do You Know Mario''s Dietary Restrictions?', 'description'),
-(2, 1, 'Do You Have What It Takes to Convince NASA to Reclassify Pluto as a Planet?', 'description');
+(1, 2, 'How Well Do You Know Mario''s Dietary Restrictions?', 'description', TRUE),
+(2, 1, 'Do You Have What It Takes to Convince NASA to Reclassify Pluto as a Planet?', 'description', TRUE);
