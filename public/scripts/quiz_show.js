d1a4ba60f9b8bbe014f43d6b9aae8875d2f3d1b1
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-05 13:33:22 -0400

    Update quiz_show

--- public/scripts/quiz_show.js
+++ public/scripts/quiz_show.js
@@ -1,11 +1,26 @@
-
+// Available data:
+//   quizData, sessionID, q
 
 $(document).ready(function() {
 
   const playButton = $("#play-quiz");
 
+  // When the user clicks play, send the quizID to the server
+  // Expected response: JSON containing the keys "sessionID", "questions"
   playButton.on("click", function() {
-    alert("clicked play!");
+    console.log($("#quiz-data").html());
+
+    // // Submit a post request with data = quizID and do NOT redirect
+    // $.ajax({
+    //   url: "/quizzes",
+    //   type: "POST",
+    //   data: quizData.id
+    // })
+    //   .then(data => {
+    //     // On successful quiz submission, redirect to the new quiz show page
+    //     console.log(data);
+    //   });
+
   })
 
 });
