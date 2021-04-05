9a6c35acdf37d76fd420891e3319fd9718164f0d
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-05 15:50:05 -0400

    Update temp reviews

--- src/routes/quizzes.js
+++ src/routes/quizzes.js
@@ -71,9 +71,9 @@ module.exports = (db) => {
           // TEMPORARY REVIEWS ///////////////
 
           const reviews = [
-            { user_id: 1, username: "reggi", comment: "this quiz sucks", rating: "1" },
-            { user_id: 2, username: "francis", comment: "this quiz depends", rating: "3" },
-            { user_id: 3, username: "pladd", comment: "this quiz is awesome", rating: "5" }
+            { user_id: 1, username: "reggi", comment: "this quiz sucks", rating: "1", timestamp: "2 days ago" },
+            { user_id: 2, username: "francis", comment: "this quiz depends", rating: "3", timestamp: "3 days ago" },
+            { user_id: 3, username: "pladd", comment: "this quiz is awesome", rating: "5", timestamp: "1 hour ago" }
           ]
 
           quizData.reviews = reviews;
