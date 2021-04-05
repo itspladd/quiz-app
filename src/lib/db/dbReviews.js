3946416a65e3d60302c2ab034a50b6cd41a9b435
Paul Ladd <vlendle@gmail.com>
2021-04-05 21:55:21 +0000

    Slight modification to ensure order by creation date

--- src/lib/db/dbReviews.js
+++ src/lib/db/dbReviews.js
@@ -6,7 +6,8 @@ module.exports = {
       SELECT reviews.*, users.username
       FROM quiz_reviews AS reviews
         JOIN users ON users.id = reviews.user_id
-      WHERE quiz_id = $1;
+      WHERE quiz_id = $1
+      ORDER BY created_at DESC;
     `;
     const queryParams = [id];
     return db.query(queryString, queryParams);
