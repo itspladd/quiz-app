3acdfdd9b9351d99107e11c4518e2de3225b3bc0
Paul Ladd <vlendle@gmail.com>
2021-04-06 14:55:47 +0000

    Fix bug in db seeds

--- src/db/seeds/03_quizzes.sql
+++ src/db/seeds/03_quizzes.sql
@@ -1,4 +1,4 @@
-INSERT INTO quizzes (author_id, category_id, title, description, public, created_)
+INSERT INTO quizzes (author_id, category_id, title, description, public, creation_time)
 VALUES
-(1, 2, 'Mario''s Dietary Restrictions', 'Do you know what Mario can and can''t eat?', TRUE),
-(2, 1, 'Make Pluto a Planet Again', 'Can you convince NASA to reclassify Pluto as a planet?', TRUE);
+(1, 2, 'Mario''s Dietary Restrictions', 'Do you know what Mario can and can''t eat?', TRUE, '2021-04-03 10:05:03'),
+(2, 1, 'Make Pluto a Planet Again', 'Can you convince NASA to reclassify Pluto as a planet?', TRUE, '2021-03-02 18:04:16');
