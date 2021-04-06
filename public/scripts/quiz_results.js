e168c9c8ed439b96036831d6cf4d49bd673dfc58
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-06 14:33:41 -0400

    Add click to reveal correct answer

--- public/scripts/quiz_results.js
+++ public/scripts/quiz_results.js
@@ -34,4 +34,11 @@ $(document).ready(function() {
 
   });
 
+  $(".results-response").on("click", function() {
+    const isCorrect = $(this).find(".results-correctness").html().trim() === "CORRECT";
+    if (!isCorrect) {
+      $(this).find(".results-actual").removeClass("d-none");
+    }
+  })
+
 });
