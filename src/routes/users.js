b1999f8d7d4fafab38a9dc3f645de562ebad1cf5
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-07 17:32:30 -0400

    Add get route for user account page

--- src/routes/users.js
+++ src/routes/users.js
@@ -31,7 +31,7 @@ module.exports = (db) => {
         for (let session of userHistory) {
           session.end_time = moment(session.end_time).format("LLLL");
         }
-        
+
         return db.getFavoritesForUser(userData.id);
       })
       .then(rows => {
@@ -48,25 +48,47 @@ module.exports = (db) => {
           userHistory,
           userFavorites
         };
-        res.render("dashboard", templateVars);
+        res.render("users_dashboard", templateVars);
       })
       .catch(err => console.error(err));
     }
   });
 
   // /users/:userid
-  router.get("/:userid", (req, res) => {
-    // const {
-    //   alerts,
-    //   userData,
-    //   currentPage
-    // } = res.locals.vars;
-    // const templateVars = {
-    //   alerts,
-    //   userData,
-    //   currentPage
-    // };
+  // Account => form to configure user info
+  router.get("/:userID", (req, res) => {
+    const {
+      alerts,
+      userData,
+      currentPage,
+      rankData
+    } = res.locals.vars;
+    // ERROR: User is not logged in
+    if (!userData) {
+      console.log("You must be logged in to do that!");
+      req.flash("warning", "You must be logged in to do that!");
+      res.redirect("/login");
+    } else {
+      // ERROR: User is logged in but trying to access with a different user ID
+      if (Number(req.params.userID) !== userData.id) {
+        console.log(req.params.userID);
+        console.log(userData.id);
+        req.flash("danger", "You don't have permission to access this page.");
+        res.redirect("/");
+      } else {
+        // SUCCESS: User is logged in and accessing their own account page
+        const templateVars = {
+          alerts,
+          userData,
+          currentPage,
+          rankData
+        };
+        res.render("users_account", templateVars);
+      }
+    }
   });
+
+
 
   return router;
 };
