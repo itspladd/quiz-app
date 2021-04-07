494d85ceb359d1941e0ba98c94ca8d0edfeba53c
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-07 16:00:50 -0400

    Add favorites tab to dashboard, forms to submit post/delete requests

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
   const quizTab = $("#tab-quizzes");
-  const historyTab = $("#tab-history");
   const quizContent = $("#user-quizzes");
+
+  const historyTab = $("#tab-history");
   const historyContent = $("#user-history");
+
+  const favoritesTab = $("#tab-favorites");
+  const favoritesContent = $("#user-favorites");
+
   let currentTab = quizTab;
   let currentContent = quizContent;
 
@@ -25,6 +30,16 @@ $(document).ready(function() {
       unselectTab(currentTab, currentContent);
       currentTab = historyTab;
       currentContent = historyContent;
+    }
+  });
+
+  // Switch to My Favorites tab
+  $("#tab-favorites").on("click", function() {
+    if (currentTab !== favoritesTab) {
+      selectTab(favoritesTab, favoritesContent);
+      unselectTab(currentTab, currentContent);
+      currentTab = favoritesTab;
+      currentContent = favoritesContent;
     }
   });
 
