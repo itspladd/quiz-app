4878017f8b7daeaf525fadbd6a70e0f018b97920
Paul Ladd <vlendle@gmail.com>
2021-04-07 18:34:02 +0000

    Add is_favorited to quiz data send on the quiz show page

--- src/routes/quizzes.js
+++ src/routes/quizzes.js
@@ -79,6 +79,14 @@ module.exports = (db) => {
           review.timestamp = utils.convertTimestamp(review.created_at);
         }
         quizData.reviews = reviewData;
+        return db.isQuizFavoritedByUser(userData.id, quiz_id);
+      })
+      .then(rows => {
+        if (rows.length > 0) {
+          quizData.is_favorited = true;
+        } else {
+          quizData.is_favorited = false;
+        }
         const templateVars = {
           alerts,
           userData,
