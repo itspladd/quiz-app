1eb50bfa7f3f0d2ed2231b25110823e15350c659
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-06 22:47:24 -0400

    Implement sort quizzes by category

--- public/scripts/quiz_index.js
+++ public/scripts/quiz_index.js
@@ -1,40 +1,57 @@
+
+// Display quizzes with the given category title
+const filterData = (data, category = "all") => {
+
+  const $browser = $("#quiz-browser");
+
+  // Clear browser
+  $browser.empty();
+
+  // Filter quizzes by the specified category
+  const filteredData = data.filter(quiz => quiz.category_title === category || category === "all");
+
+  for (const quiz of filteredData) {
+    // Create quiz list item
+    const $quizItem = $(`
+      <a href="/quizzes/${quiz.id}">
+        <div class="d-flex flex-row justify-content-between align-items-center list-quiz">
+          <div class="col-8">
+            <p class="quiz-title mb-0 mt-0">${quiz.title}</p>
+            <p class="quiz-desc d-none d-xl-inline">${quiz.description}</p>
+            <p class="quiz-author d-none d-md-block my-1 text-muted">Submitted by ${quiz.author}</p>
+          </div>
+          <img class="quiz-go pr-3" src="/images/arrow-green.png" alt="arrow" />
+        </div>
+      </a>
+    `);
+    // Add quiz to browser container
+    $browser.append($quizItem);
+  }
+
+};
+
 $(document).ready(function() {
 
   // Get EJS data
-  const ejsData = JSON.parse($("#ejs").attr("data-ejs"));
+  const quizData = JSON.parse($("#ejs").attr("data-ejs"));
 
-  console.log(ejsData);
+  let category = "all";
 
-  const quizTab = $("#tab-quizzes");
-  const historyTab = $("#tab-history");
-  const quizContent = $("#user-quizzes");
-  const historyContent = $("#user-history");
-  let currentTab = quizTab;
-  let currentContent = quizContent;
+  // Show all quizzes initially
+  filterData(quizData, category);
 
-  // Switch to My Quizzes tab (default)
-  $("#tab-quizzes").on("click", function() {
-    if (currentTab !== quizTab) {
-      selectTab(quizTab, quizContent);
-      unselectTab(currentTab, currentContent);
-      currentTab = quizTab;
-      currentContent = quizContent;
+  $(".tab-btn").on("click", function(event) {
+    const $target = $(event.target);
+    if ($target.is("#tab-all")) {
+      category = "all"
+    } else if ($target.is("#tab-tech")) {
+      category = "Technology"
+    } else if ($target.is("#tab-gaming")) {
+      category = "Gaming"
+    } else if ($target.is("#tab-edu")) {
+      category = "Education"
     }
-
-  });
-
-  // Switch to My History tab
-  $("#tab-history").on("click", function() {
-    if (currentTab !== historyTab) {
-      selectTab(historyTab, historyContent);
-      unselectTab(currentTab, currentContent);
-      currentTab = historyTab;
-      currentContent = historyContent;
-    }
-  });
-
-  $("#test").on("click", function() {
-    console.log($(this).attr("data-ejs"));
-  });
+    filterData(quizData, category);
+  })
 
 });
