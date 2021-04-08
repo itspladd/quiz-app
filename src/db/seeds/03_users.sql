d2593f0474a5504042f39cef15930f8f7312a948
Paul Ladd <vlendle@gmail.com>
2021-04-08 01:22:04 +0000

    Add admin user, featured column for quizzes, and new quiz about SQL

--- src/db/seeds/03_users.sql
+++ src/db/seeds/03_users.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (username, password, email, avatar_id)
 VALUES
+('admin', '$2b$10$v1MurxaJlicWR81MBQ8yJ.0.aJdt1rXtWHXA75c9La4W0zePVo9J2', 'admin@admin.admin', 1),
 ('pladd', '$2b$10$.rphMkSBYWcHW68h4nWlsOxbojC8w/z8xbhNicqJLM1LgnMcppOvu', 'pladd@radd.cad', 1),
 ('reggi', '$2b$10$.rphMkSBYWcHW68h4nWlsOxbojC8w/z8xbhNicqJLM1LgnMcppOvu', 'regg@egg.megg', 2),
 ('timid_terry', '$2b$10$.rphMkSBYWcHW68h4nWlsOxbojC8w/z8xbhNicqJLM1LgnMcppOvu', 'terry@mary.berry', 5);
