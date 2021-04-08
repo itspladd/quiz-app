51b7ad2f909186957b99f9a723633969c851e07c
Paul Ladd <vlendle@gmail.com>
2021-04-08 16:14:30 +0000

    Remove manually-entered creation time for seed quizzes

--- src/db/seeds/04_quizzes.sql
+++ src/db/seeds/04_quizzes.sql
@@ -1,6 +1,6 @@
-INSERT INTO quizzes (author_id, category_id, title, description, public, creation_time)
+INSERT INTO quizzes (author_id, category_id, title, description, public)
 VALUES
-(2, 2, 'Mario''s Dietary Restrictions', 'Do you know what Mario can and can''t eat?', TRUE, '2021-04-03 10:05:03'),
-(3, 1, 'Make Pluto a Planet Again', 'Can you convince NASA to reclassify Pluto as a planet?', TRUE, '2021-03-02 18:04:16'),
-(1, 3, 'PostgreSQL', 'The most (data)based quiz around.', TRUE, '2021-04-07 20:08:15')
+(2, 2, 'Mario''s Dietary Restrictions', 'Do you know what Mario can and can''t eat?', TRUE),
+(3, 1, 'Make Pluto a Planet Again', 'Can you convince NASA to reclassify Pluto as a planet?', TRUE),
+(1, 3, 'PostgreSQL', 'The most (data)based quiz around.', TRUE')
 ;
