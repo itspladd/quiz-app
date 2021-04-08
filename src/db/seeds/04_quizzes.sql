9af199a7554bee24cf86fca6c535a21cd3458986
Paul Ladd <vlendle@gmail.com>
2021-04-08 16:19:39 +0000

    Fix typo in quiz seed

--- src/db/seeds/04_quizzes.sql
+++ src/db/seeds/04_quizzes.sql
@@ -2,5 +2,5 @@ INSERT INTO quizzes (author_id, category_id, title, description, public)
 VALUES
 (2, 2, 'Mario''s Dietary Restrictions', 'Do you know what Mario can and can''t eat?', TRUE),
 (3, 1, 'Make Pluto a Planet Again', 'Can you convince NASA to reclassify Pluto as a planet?', TRUE),
-(1, 3, 'PostgreSQL', 'The most (data)based quiz around.', TRUE')
+(1, 3, 'PostgreSQL', 'The most (data)based quiz around.', TRUE)
 ;
