6578b638017249a0aa19f01ad51cd7741a4e9de3
Paul Ladd <vlendle@gmail.com>
2021-04-08 20:46:02 +0000

    Added documentation for favorites functions

--- src/lib/db/dbFavorites.js
+++ src/lib/db/dbFavorites.js
@@ -2,10 +2,27 @@ const { query } = require("./db");
 const db = require("./db");
 
 module.exports = {
+  
+  /**
+   * Add a new entry to the 'favorites' table.
+   * @param  {Object} favoriteData
+   *         Must contain { user_id, quiz_id }
+   * @return {Object}
+   *         A promise to the new favorite as an object.
+   *
+   */
   addFavorite: function(favoriteData) {
     return db.insert("favorites", favoriteData);
   },
 
+  /**
+   * Delete an entry from the 'favorites' table, given the user and the quiz they wish to un-favorite.
+   * @param  {Object} favoriteData
+   *         Must contain { user_id, quiz_id }
+   * @return {Object}
+   *         A promise to the deleted favorite as an object.
+   *
+   */
   deleteFavorite: function(favoriteData) {
     const queryString = `
       DELETE 
@@ -19,6 +36,14 @@ module.exports = {
     return db.query(queryString, queryParams);
   },
 
+  /**
+   * Check to see if an entry matching the given user_id and quiz_id exists in the 'favorites' table.
+   * @param  {Object} favoriteData
+   *         Must contain { user_id, quiz_id }
+   * @return {[Object]}
+   *         A promise to the favorite as an object.
+   *
+   */
   isQuizFavoritedByUser: function(user_id, quiz_id) {
     const queryString = `
       SELECT * FROM favorites
@@ -29,6 +54,14 @@ module.exports = {
     return db.query(queryString, queryParams);
   },
 
+  /**
+   * Retrieve all of a user's favorited quizzes from the database.
+   * @param  {Object} favoriteData
+   *         Must contain { user_id, quiz_id }
+   * @return {[Object]}
+   *         A promise to an arrayof objects containing quiz data, including cover photo URLS.
+   *
+   */
   getFavoritesForUser: function(user_id) {
     const queryString = `
       SELECT quizzes.*,
